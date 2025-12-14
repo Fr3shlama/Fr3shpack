187e342bcfd806a4d0d461a31373c02b061ca090
Fr3shlama <root@lama.host>
2025-12-14 19:22:04 +0100

    added inginite lava cell

--- kubejs/startup_scripts/ExtendedAE/infinite_lava_cell.js
+++ kubejs/startup_scripts/ExtendedAE/infinite_lava_cell.js
@@ -1,8 +1,6 @@
 StartupEvents.registry("item", (event) => {
-  const item = event.create("infinity_lava_cell", "custom_infinity_cell");
-  // TODO: texture and cell model
-  item.texture("appliedenergistics2:item/cell_creative");
-  item.cellModel("appliedenergistics2:block/drive/cell_creative");
-
-  item.fluidType("minecraft:lava");
-});
+  event.create("infinity_lava_cell", "custom_infinity_cell")
+    .texture("kubejs:item/infinity_lava_cell")
+    .fluidType("minecraft:lava")
+    .cellModel('kubejs:block/infinity_lava_cell')
+})
