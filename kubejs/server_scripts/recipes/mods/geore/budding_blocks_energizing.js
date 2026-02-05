a58dffee98ab26c6789bff692f272cb5f3a7d2a1
Fr3shlama <root@lama.host>
2026-02-05 01:36:03 +0100

    migrate  budding recipes to other mods

--- kubejs/server_scripts/recipes/mods/geore/budding_blocks_energizing.js
+++ kubejs/server_scripts/recipes/mods/geore/budding_blocks_energizing.js
@@ -1,40 +1,9 @@
-// Fr3shlama (TM) | File: kubejs/server_scripts/recipes/mods/geore/budding_blocks_energizing.js
-// Description: Add Powah energizing recipes to create GeOre budding blocks.
-/*
-ServerEvents.recipes(event => {
-
-  const FE_COST = 100000; 
-
+/* TODO 
   const materials = [
-    'emerald',
-    'diamond',
-    'gold',
-    'iron',
-    'copper',
-    'redstone',
-    'lapis',
-    'quartz',
-    'ancient_debris',
-    'aluminum',
-    'tin',
-    'silver',
-    'uranium',
-    'platinum',
-    'osmium',
-    'nickel',
-    'lead',
-    'zinc',
-    'coal'
+    'emerald', beryllium superheated mixer
+    'diamond', implosion compressor 1 stack 5x coal
+    'lapis',  sliceNdice 2x blue dye 1x geoOre 3x prismarine
+    'quartz', crystal fixer
+    'uranium', irradiation
   ];
-
-  materials.forEach(material => {
-    event.recipes.powah.energizing(
-      [`geore:${material}_block`],      
-      `geore:budding_${material}`,       
-      FE_COST                            
-    ).id(`pack:energize_${material}_budding`);
-  });
-
-});
-
 */
