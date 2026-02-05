d7bc59911ed009e5ec467bbd0d3c396f8ec47f81
Fr3shlama <root@lama.host>
2026-02-05 22:20:22 +0100

    enabled ID tweaks again

--- kubejs/server_scripts/recipes/removals/integrated_dynamics_ore_squeezer.js
+++ kubejs/server_scripts/recipes/removals/integrated_dynamics_ore_squeezer.js
@@ -1,11 +1,10 @@
 // Fr3shlama (TM) | File: kubejs/server_scripts/recipes/removals/integrated_dynamics_ore_squeezer.js
 // Description: Remove Integrated Dynamics ore squeezer recipes.
 
-// Disabled due to mod removal
-//ServerEvents.recipes((event) => {
+ServerEvents.recipes((event) => {
 	// Remove ore processing in Squeezer
-//	event.remove({ id: /integrateddynamics:squeezer\/ore\/.*/ })
-//	event.remove({ id: /integrateddynamics:mechanical_squeezer\/ore\/.*/ })
-// })
+	event.remove({ id: /integrateddynamics:squeezer\/ore\/.*/ })
+	event.remove({ id: /integrateddynamics:mechanical_squeezer\/ore\/.*/ })
+})
 
 
